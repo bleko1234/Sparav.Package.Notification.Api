27ebab0a97a8f318362c3d4e4b9c99f5ab468028
Blerim Cazimi <blerim@burd.dk>
2020-09-04 10:45:04 +0200

    Update PushNotificationTokenClientV1.php

--- PushNotificationTokenClientV1.php
+++ PushNotificationTokenClientV1.php
@@ -11,14 +11,14 @@ class PushNotificationTokenClientV1
     /**
      * @param string $token
      * @param int $customer_id
-     * @param string $device_type
+     * @param int $device
      * @return Response
      */
-    public function addToken(string $token, int $customer_id, string $device_type)
+    public function addToken(string $token, int $customer_id, int $device)
     {
         $response = Http::timeout(15)
             ->withBasicAuth(env('SPARAV_NOTIFICATION_API_AUTH_USERNAME'), env('SPARAV_NOTIFICATION_API_AUTH_PASSWORD'))
-            ->post('https://sparavnotificationapiprod.azurewebsites.net/api/v1/pushnotification/token', ['token' => $token, 'customer_id' => $customer_id, 'device_type' => $device_type]);
+            ->post('https://sparavnotificationapiprod.azurewebsites.net/api/v1/pushnotification/token', ['token' => $token, 'customer_id' => $customer_id, 'device' => $device]);
         return $response;
     }
 
