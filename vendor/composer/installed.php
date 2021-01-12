f1f56c209fa22aa434c64b91672d53292fca4b69
blerimcazimi <blerim@tandbud.dk>
2021-01-12 12:22:25 +0100

    ..

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.4',
+    'version' => '1.0.4.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'sparav/notification',
+  ),
+  'versions' => 
+  array (
+    'sparav/notification' => 
+    array (
+      'pretty_version' => '1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
